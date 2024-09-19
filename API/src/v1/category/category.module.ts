83e89d59811ec3b848e3232821deb0db8e1ee33c
yslaa <crislhan.fabila@tup.edu.ph>
2024-09-19 19:27:53 +0800

    feat: Add Category Logic

---
+++ API/src/v1/category/category.module.ts
@@ -0,0 +1,11 @@
+// category.module.ts
+import { Module } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
+
+@Module({
+  imports: [],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+})
+export class CategoryModule {}
